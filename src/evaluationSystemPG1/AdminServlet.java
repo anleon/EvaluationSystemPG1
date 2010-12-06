c2a62b580663d2f05effae56a888da58b426bef8
Sten <sten-ake.strid@hotmail.com>
2010-12-06 21:05:24 +0100

    Split the URL and forward to some of the jsp files.

--- src/evaluationSystemPG1/AdminServlet.java
+++ src/evaluationSystemPG1/AdminServlet.java
@@ -2,6 +2,7 @@ package evaluationSystemPG1;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -17,6 +18,8 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 
 import evaluationSystemPG1.db.HibernateUtil;
+import evaluationSystemPG1.entities.EvalTemplate;
+import evaluationSystemPG1.entities.EvalTemplateDAO;
 
 /**
  * Servlet implementation class Guestbook
@@ -43,7 +46,64 @@ public class AdminServlet extends HttpServlet {
 			guestSession = new GuestbookSession();
 		}*/		
 		init(this.getServletContext());
-		request.getRequestDispatcher("/QuestionTest.jsp").include(request, response);
+		/*
+		 * These pages are handled by this servlet:
+		 * /Login
+		 * /Evaluations
+		 * /Evaluations/{id}
+		 * /Groups
+		 * /Groups/{id}
+		 * 
+		 * /Evaluations/Create
+		 * /Groups/Create
+		 * 
+		 * */
+		String s = request.getRequestURI();
+		String[] ss = s.split("/");
+	
+		// String projectName = ss[1];
+		// String servletName = ss[2];
+		String pageName = "";
+		String identifier = "";
+		if (ss.length > 3) {
+			pageName = ss[3];
+		}
+		if (ss.length > 4) {
+			identifier = ss[4];
+		}
+		
+		if ("Evaluations".equals(pageName)) {
+			pageEval(request,response,identifier);
+		} else if ("Groups".equals(pageName)) {
+			request.getRequestDispatcher("/Groups.jsp").forward(request, response);
+		} else {
+			request.getRequestDispatcher("/Login.jsp").forward(request, response);
+		}
+		
+		
+		Writer wr = response.getWriter();
+		wr.write(pageName + "<br />");
+		for(String string: ss) {
+			wr.write(string);
+			wr.write(",");
+		}
+		
+	}
+
+	private void pageEval(HttpServletRequest request, HttpServletResponse response, String identifier) throws ServletException, IOException {
+		int id;
+		try {
+			id = Integer.parseInt(identifier);
+		} catch (Exception e) {
+			id = 0;
+		}
+		if (id == 0) {
+			request.getRequestDispatcher("EvalTemplates.jsp").forward(request, response);
+		} else {
+			EvalTemplate et = EvalTemplateDAO.getEvalTemplate(id);
+			request.setAttribute("EvalTemplate", et);
+			request.getRequestDispatcher("EvalTemplate.jsp").forward(request, response);
+		}
 	}
 
 	/**
