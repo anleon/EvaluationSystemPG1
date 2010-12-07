f06c11bc70f53523c1080dedb4d2ccd468d30ac1
anleon <alexander@leonov.se>
2010-12-07 13:25:33 +0100

    continue with js addAlternative.js

--- WebContent/addAlternative.js
+++ WebContent/addAlternative.js
@@ -7,7 +7,9 @@ $(document).ready(function(){
 		$('body').append('<label for="section_title">Sektions titel</label><input type="text" id="section_title" name="section_title" /><input type="submit" name="delete_section" value="Ta bort" /><label for="section_title">Sektions beskrivning</label><input type="text" id="section_description" name="section_description" />');
 	});
 	$('input[name="create_question"]').click(function() {
+		var questionCounter = $('div[id^="question_div"]').length;
+		questionCounter++;
 		//console.log(this);
-		$('body').append('<label for="question">Fråga:</label><input type="text" id="question" name="question" /><input type="submit" name="delete_question" value="Ta bort" />');
+		$('body').append('<div id="question_div'+questionCounter+'"><label for="question_lable'+questionCounter+'">Fråga '+questionCounter+':</label><input type="text" id="question'+questionCounter+'" name="question'+questionCounter+'" /><input type="submit" name="delete_question'+questionCounter+'" value="Ta bort" /></div>');
 	});
 });
