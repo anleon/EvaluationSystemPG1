02beba83d13ea317b4cd14f0d7389c86ac67970e
anleon <alexander@leonov.se>
2010-12-07 13:39:09 +0100

    fix with js append

--- WebContent/addAlternative.js
+++ WebContent/addAlternative.js
@@ -4,7 +4,7 @@
 $(document).ready(function(){
 	$('input[name="create_section"]').click(function() {
 		//console.log(this);
-		$('body').append('<label for="section_title">Sektions titel</label><input type="text" id="section_title" name="section_title" /><input type="submit" name="delete_section" value="Ta bort" /><label for="section_title">Sektions beskrivning</label><input type="text" id="section_description" name="section_description" />');
+		$('form').append('<label for="section_title">Sektions titel</label><input type="text" id="section_title" name="section_title" /><input type="submit" name="delete_section" value="Ta bort" /><label for="section_title">Sektions beskrivning</label><input type="text" id="section_description" name="section_description" />');
 	});
 	$('input[name="create_question"]').click(function() {
 		var questionCounter = $('div[id^="question_div"]').length;
--- WebContent/main.js
+++
@@ -1,6 +0,0 @@
-/**
- * 
- */
-$(document).ready(function(){
-	$("input[submit]").setAttr("disable","disable")
-}
