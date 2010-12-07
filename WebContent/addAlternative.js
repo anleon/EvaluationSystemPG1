9d746e6b3c187d6274a6523a6638bd97f9356cff
anleon <alexander@leonov.se>
2010-12-07 17:00:59 +0100

    near finnish with addAlternative.js

--- WebContent/addAlternative.js
+++ WebContent/addAlternative.js
@@ -11,21 +11,36 @@ $(document).ready(function(){
 	var divTag = '<div></div>';
 	var sectionCounter = 0;
 	var questionCounter = 0;
-	$('input[name="create_section"]').click(function(labelTag,inputTextTag,inputButtonTag,inputSubmitTag,divTag,sectionCounter) {
-		var idAttr = $('div[id^="section_div"]').attr('id');
-		sectionCounter = parsInt(idAttr);
-		sectionCounter++;
-		$(labelTag).attr('for','section_title"+sectionCounter+"').text('Sektions titel');
-		$(inputTextTag).attr()
-		$(divTag).append();
-		$('form').append('<div class="section_div"><label for="section_title">Sektions titel</label><input type="text" id="section_title" name="section_title" /><input type="submit" name="delete_section" value="Ta bort" /><label for="section_title">Sektions beskrivning</label><input type="text" id="section_description" name="section_description" /></div>');
-	});
-	$('input[name="create_question"]').click(function() {
-		var idAttr = $('div[id^="question_div"]').attr('id');
-		console.log(idAttr);
+	var idAttr = '';
+	$('input[name="create_section"]').click(function(labelTag,inputTextTag,inputButtonTag,inputSubmitTag,divTag,sectionCounter,idAttr) {
+		idAttr = $('div[id^="section_div"]').last().attr('id');
+		if(idAttr == undefined){
+			sectionCounter = 1;
+		}else{
+			sectionCounter = idAttr.slice(11);
+			sectionCounter++;
+		}
+		console.log(sectionCounter);
+		$('form').append('<div id="section_div'+sectionCounter+'"></div>');
+		$('form #section_div'+sectionCounter).append('<label for="section_title'+sectionCounter+'">Sektions titel '+sectionCounter+'</label>');
+		$('form #section_div'+sectionCounter).append('<input type="text" id="section_title'+sectionCounter+'" name="section_title'+sectionCounter+'" />');
+		$('form #section_div'+sectionCounter).append('<input type="submit" name="delete_section'+sectionCounter+'" value="Ta bort" />');
+		$('form #section_div'+sectionCounter).append('<label for="section_description'+sectionCounter+'">Sektions beskrivning</label>');
+		$('form #section_div'+sectionCounter).append('<input type="text" id="section_description'+sectionCounter+'" name="section_description'+sectionCounter+'" />');
 		
-		questionCounter = idAttr.slice(12);
-		questionCounter++;
+//		labelTag = $(labelTag).attr('for','section_title"+sectionCounter+"').text('Sektions titel "+sectionCounter+"');
+		//$(inputTextTag).attr();
+		//$(divTag).append(labelTag);
+//		$('form').append('<div id="section_div'+sectionCounter+'"><label for="section_title">Sektions titel'+sectionCounter+'</label><input type="text" id="section_title" name="section_title" /><input type="submit" name="delete_section" value="Ta bort" /><label for="section_description">Sektions beskrivning</label><input type="text" id="section_description" name="section_description" /></div>');
+	});
+	$('input[name="create_question"]').click(function(idAttr) {
+		idAttr = $('div[id^="question_div"]').last().attr('id');
+		if(idAttr == undefined){
+			questionCounter = 1;
+		}else{
+			questionCounter = idAttr.slice(12);
+			questionCounter++;
+		}
 		//console.log(this);
 		$('form').append('<div id="question_div'+questionCounter+'"><label for="question'+questionCounter+'">Fråga '+questionCounter+':</label><input type="text" id="question'+questionCounter+'" name="question'+questionCounter+'" /><input type="submit" name="delete_question'+questionCounter+'" value="Ta bort" /><input type="checkbox" id="question_chbx_freetext'+questionCounter+'" name="question_chbx_freetext'+questionCounter+'" /><label for="question_chbx_freetext'+questionCounter+'">Fritext</label></div>');
 	});
