895c82eb4b8c4a87766e987c900ad104e5ff04b6
anleon <alexander@leonov.se>
2010-12-08 12:14:22 +0100

    addAlternative.js allmost finished, question lacks multiple_choice

--- WebContent/addAlternative.js
+++ WebContent/addAlternative.js
@@ -17,27 +17,43 @@ $(document).ready(function(){
 		if(idAttr == undefined){
 			sectionCounter = 1;
 		}else{
-			sectionCounter = idAttr.slice(11);
+			sectionCounter = idAttr.slice(12);
 			sectionCounter++;
 		}
 		//console.log(sectionCounter);
-		$('form').append('<div id="section_div'+sectionCounter+'"></div>');
-		$('form #section_div'+sectionCounter).append('<label for="section_title'+sectionCounter+'">Sektions titel '+sectionCounter+'</label>');
-		$('form #section_div'+sectionCounter).append('<input type="text" id="section_title'+sectionCounter+'" name="section_title'+sectionCounter+'" />');
-		$('form #section_div'+sectionCounter).append('<input type="submit" name="delete_section'+sectionCounter+'" value="Ta bort" />');
-		$('form #section_div'+sectionCounter).append('<label for="section_description'+sectionCounter+'">Sektions beskrivning</label>');
-		$('form #section_div'+sectionCounter).append('<textarea type="text" id="section_description'+sectionCounter+'" name="section_description'+sectionCounter+'"></textarea>');
+		$('form').append('<div class="section" id="section_div_'+sectionCounter+'"></div>');
+		$('#section_div_'+sectionCounter).append('<label for="section_title_'+sectionCounter+'">Sektions titel '+sectionCounter+'</label>');
+		$('#section_div_'+sectionCounter).append('<input type="text" id="section_title_'+sectionCounter+'" name="section_title_'+sectionCounter+'" />');
+		$('#section_div_'+sectionCounter).append('<input type="submit" name="delete_section_'+sectionCounter+'" value="Ta bort" />');
+		$('#section_div_'+sectionCounter).append('<label for="section_description_'+sectionCounter+'">Sektions beskrivning</label>');
+		$('#section_div_'+sectionCounter).append('<textarea type="text" id="section_description_'+sectionCounter+'" name="section_description_'+sectionCounter+'"></textarea>');
 		
 	});
 	$('input[name="create_question"]').click(function(idAttr) {
-		idAttr = $('div[id^="question_div"]').last().attr('id');
+		idAttr = $('div[id^="question_div_"]').last().attr('id');
 		if(idAttr == undefined){
 			questionCounter = 1;
 		}else{
-			questionCounter = idAttr.slice(12);
+			questionCounter = idAttr.slice(13);
 			questionCounter++;
 		}
 		//console.log(this);
-		$('form').append('<div id="question_div'+questionCounter+'"><label for="question'+questionCounter+'">Fråga '+questionCounter+':</label><input type="text" id="question'+questionCounter+'" name="question'+questionCounter+'" /><input type="submit" name="delete_question'+questionCounter+'" value="Ta bort" /><input type="checkbox" id="question_chbx_freetext'+questionCounter+'" name="question_chbx_freetext'+questionCounter+'" /><label for="question_chbx_freetext'+questionCounter+'">Fritext</label></div>');
+		$('form').append('<div class="question" id="question_div_'+questionCounter+'"></div>');
+		$('#question_div_'+questionCounter).append('<label for="question_'+questionCounter+'"><h4>Fråga '+questionCounter+':</h4></label>');
+		$('#question_div_'+questionCounter).append('<textarea type="text" id="question_'+questionCounter+'" name="question_'+questionCounter+'"></textarea>');
+		$('#question_div_'+questionCounter).append('<input type="submit" name="delete_question_'+questionCounter+'" value="Ta bort" />');
+		$('#question_div_'+questionCounter).append('<fieldset></fieldset>');
+		$('#question_div_'+questionCounter+' fieldset').append('<input type="checkbox" id="free_text_'+questionCounter+'" name="option_types_'+questionCounter+'" checked="checked" />');
+		$('#question_div_'+questionCounter+' fieldset').append('<label for="free_text_'+questionCounter+'">Fri text</label>');
+		$('#question_div_'+questionCounter+' fieldset').append('<input type="checkbox" id="alternatives_'+questionCounter+'" name="option_types_'+questionCounter+'" checked="checked" />');
+		$('#question_div_'+questionCounter+' fieldset').append('<label for="alternatives_'+questionCounter+'">Fritext</label>');
+		$('#question_div_'+questionCounter).append('<fieldset></fieldset>');
+		$('#question_div_'+questionCounter+' fieldset:last').append('<input type="radio" id="single_choice_'+questionCounter+'" name="option_type_'+questionCounter+'" value="Enval" />');
+		$('#question_div_'+questionCounter+' fieldset:last').append('<label for="single_choice_'+questionCounter+'">Enval</label>');
+		$('#question_div_'+questionCounter+' fieldset:last').append('<input type="radio" id="multiple_choice_'+questionCounter+'" name="option_type_'+questionCounter+'" value="Flerval" />');
+		$('#question_div_'+questionCounter+' fieldset:last').append('<label for="multiple_choice_'+questionCounter+'">Flerval</label>');
+		$('#question_div_'+questionCounter+' fieldset:last').append('<input type="radio" id="scale_'+questionCounter+'" name="option_type_'+questionCounter+'" checked="checked" value="1-6" />');
+		$('#question_div_'+questionCounter+' fieldset:last').append('<label for="scale_'+questionCounter+'">1-6</label>');
+		
 	});
 });
