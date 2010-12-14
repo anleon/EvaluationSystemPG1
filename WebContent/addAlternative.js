541998f508197641de40ae8d6ac80f76a6a65d01
Sten <sten-ake.strid@hotmail.com>
2010-12-14 09:36:36 +0100

    Changed to a json variant for the population js script.

--- WebContent/addAlternative.js
+++ WebContent/addAlternative.js
@@ -177,6 +177,52 @@ $(document).ready(function(){
 		$("body").append(script);
 	}
 
-	inc("jstest.js");
+	inc("jstest2.js");
+	
+	
+	eval_json = {
+			"title" : "En fin titel",
+			"group" : "0",
+			"eval_component" : [
+                {
+                	"type" : "section",
+                	"title" : "Fin Sektionstitel",
+                	"description" : "En mycket fin beskrivning för den här sektionen av frågor"
+                } , 
+                {
+                	"type" : "question",
+                	"description" : "AA textOption och radiobutton1to6",
+                		"options" : {"choice" : {}}
+                } , 
+                {
+                	"type" : "question",
+                	"description" : "BBBBBBBBBBBBBBBBB",
+                	"options" : {
+                		"free_text" : false,
+                		"choice" : {"single_choice" : {"alternatives": ["dåligt","ok","bra","utmärkt"]}}		
+                	}
+                } ,
+                {
+                	"type" : "question",
+                	"description" : "CCCCCCCCCCCCCCCCCCCC",
+                	"mandatory" : false,
+                	"options" : {
+                		"free_text" : false,
+                		"choice" : {"multiple_choice" : {"alternatives": ["utåtriktad","glad","målfokuserad"]}}		
+                	}
+                } ,
+                {
+                	"type" : "question",
+                	"description" : "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD",
+                	"mandatory" : false,
+                	"options" : {
+                		"choice_set" : false,
+                		"choice" : {"multiple_choice" : {"alternatives": ["utåtriktad","glad","målfokuserad"]}}		
+                	}
+                }
+          ]
+	};
+	
+	populate(eval_json);
 		
 });
